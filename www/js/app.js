eab8ffccd94b557878a95c82038f6608a216ecdd
mnmpower <mnmmaarten@hotmail.com>
2019-02-23 14:48:04 +0100

    completed JS for dicereoller, need to add more dices on HTML

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,11 @@
 $(function() {
     document.addEventListener("deviceready", onDeviceReady, false);
+    $('#dicebutton').click(function () {
+        Dice.init();
+        console.log('Dice.js is loaded');
+    })
 });
 
 function onDeviceReady() {
     console.log('Device is ready');
-    Dice.init();
 };
--- www/js/dice.js
+++ www/js/dice.js
@@ -1,9 +1,64 @@
 var Dice = function () {
     var init = function () {
-        console.log('dice');
+        console.log('dice funtion rolls started');
+        //DICE ROLLING
+        //
+        //START
+
+        //STOP
 
     };
     return {
         init: init
     };
 }();
+
+$( document ).ready(function() {
+    console.log('dice funtion rolls started');
+
+    $('.dice').click(function () {
+
+        //DECLAREREN
+        var resultaat="";
+        var dropbox;
+        var aantalDice;
+        var i;
+        var nummer;
+
+        //INLEZEN DROPBOX
+        var rolldXid= $(this).attr('id');
+        console.log(rolldXid);
+
+        var getalvanklickedid = rolldXid.substr(5,2);
+        console.log(getalvanklickedid);
+
+        var dropboxid = "amountD"+ getalvanklickedid;
+        console.log(dropboxid);
+
+        dropbox = document.getElementById(dropboxid);
+        aantalDice = dropbox.options[dropbox.selectedIndex].value;
+        console.log(aantalDice + " dices");
+
+        //ROLLS SIMULEREN
+        for (i = 1; i <= aantalDice; i++) {
+            nummer = Math.floor(Math.random() * getalvanklickedid) + 1;
+            console.log("roll: "+nummer);
+
+            //resultaat opvullen
+            if (!(i == aantalDice)){
+                resultaat += nummer + ", ";
+            } else {
+                resultaat += nummer;
+            }
+
+        }
+        //JUISTE ID MAKEN VOOR INPUTFIELD
+        var inputid = "input"+getalvanklickedid;
+        console.log(inputid);
+
+        //INPUTFIELD INLIEZE
+        $('#'+inputid).val(resultaat);
+        //document.getElementById(inputid);
+
+    });
+});
