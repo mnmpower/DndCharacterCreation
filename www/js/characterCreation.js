6550fdab1870abe24c47e24e28bbab06183deaab
mnmpower <mnmmaarten@hotmail.com>
2019-03-05 19:21:01 +0100

    finished content of character creation.html

--- www/js/characterCreation.js
+++ www/js/characterCreation.js
@@ -9,29 +9,52 @@ $(document).ready(function () {
            //$('.tabs').tabs('select','Melee');
         }
     });
+    //alles met de CLASSES
+    var jsonclasses = leesJsonIn("../data/classes.json");
+    console.log(jsonclasses);
 
-    var json = leesJsonIn();
-    console.log(json);
-
-    var classNames = Object.keys(json);
+    var classNames = Object.keys(jsonclasses);
     console.log(classNames);
 
     var melee=[];
     var specialist=[];
     var spellcaster=[];
 
-    sortClassByStyle(classNames,json,melee,specialist,spellcaster);
-    leesTab2In(json,melee,specialist,spellcaster);
+    sortClassByStyle(classNames,jsonclasses,melee,specialist,spellcaster);
+    leesClassTabIn(jsonclasses,melee,specialist,spellcaster);
 
 
+    //alles met de RACES
+    var jsonRaces = leesJsonIn("../data/races.json");
+    console.log(jsonRaces);
+
+    var raceNames = Object.keys(jsonRaces);
+    console.log(raceNames);
+    leesRaceTabsIn(raceNames,jsonRaces);
+
+    var gekozenRace="Human";
+    var gekozenClass="Barbarian";
+
+    $('.race').click(function () {
+        gekozenRace = $(this).text();
+        console.log(gekozenRace);
+        console.log(gekozenClass);
+    });
+    $('.class').click(function () {
+        gekozenClass = $(this).text();
+        console.log(gekozenRace);
+        console.log(gekozenClass);
+    });
+    console.log(gekozenRace);
+    console.log(gekozenClass);
 });
 
-function leesJsonIn() {
+function leesJsonIn(url) {
     var json = null;
     $.ajax({
         "async": false,
         "global": false,
-        "url": "../data/classes.json",
+        "url": url,
         "dataType": "json",
         "success": function (data) {
             json = data;
@@ -40,11 +63,11 @@ function leesJsonIn() {
     return json;
 }
 
-function sortClassByStyle(classNames,json,melee,specialist,spellcaster) {
+function sortClassByStyle(classNames,jsonclasses,melee,specialist,spellcaster) {
 
     for ( var i = 0; i < classNames.length; i++) {
 
-        var fightingstyle = json[classNames[i]]["ClassFeatures"]["FightingStyle"];
+        var fightingstyle = jsonclasses[classNames[i]]["ClassFeatures"]["FightingStyle"];
 
         console.log(fightingstyle);
 
@@ -61,34 +84,64 @@ function sortClassByStyle(classNames,json,melee,specialist,spellcaster) {
     console.log(spellcaster);
 }
 
-function leesTab2In(json,melee,specialist,spellcaster) {
+function leesClassTabIn(jsonclasses,melee,specialist,spellcaster) {
     $('a.selectFightingStyle').click(function () {
         var selectedType = $(this).text();
-        var tabs2titles = [];
+        var tabstitles = [];
         console.log(selectedType);
         //nakijken welke tab geklikt is + jusite date doorgeven
         switch (selectedType) {
             case "Specialist":
-                tabs2titles = specialist;
+                tabstitles = specialist;
                 break;
 
             case "Spellcaster":
-                tabs2titles = spellcaster;
+                tabstitles = spellcaster;
                 break;
 
             default:
-                tabs2titles = melee;
+                tabstitles = melee;
                 break;
         }
-        console.log(tabs2titles);
+        console.log(tabstitles);
 
         //inlezen tabs
-        for (var i = 0; i < tabs2titles.length; i++) {
-            $('#class'+(i+1)).html(tabs2titles[i]);
-            $('#class'+(i+1)+"uitleg").html(json[tabs2titles[i]]["ClassFeatures"]["Class Description"]);
-            console.log(tabs2titles[i]);
+        for (var i = 0; i < tabstitles.length; i++) {
+            $('#class'+(i+1)).html(tabstitles[i]);
+            $('#class'+(i+1)+"uitleg").html(jsonclasses[tabstitles[i]]["ClassFeatures"]["Class Description"]);
+            console.log(tabstitles[i]);
         }
     });
+}
+//werkt perfect maar ff test
+// function leesRaceTabsIn(racenames,jsonRaces) {
+//     for (var i = 1; i <= racenames.length; i++) {
+//         $('#race'+(i)).html(racenames[i]);
+//         $('#race'+(i)+"uitleg").html(jsonRaces[racenames[i]][racenames[i]+" Traits"]["content"]);
+//         console.log(racenames[i]);
+//     }
+// }
+
+//afdrukken zonder iets nodig te hebbe in HTML :D
+function leesRaceTabsIn(racenames,jsonRaces) {
+    var resultaat="<div class=\"col s12\">\n<ul class=\"tabs\">\n";
+
+
+    for (var i = 1; i < racenames.length; i++) {
+
+        resultaat+="<li class=\"tab\"><a href=\"#race"+i+"uitleg\" id=\"race"+i+"\" class='race'>"+racenames[i]+"</a></li>\n";
+
+    }
+
+    resultaat+="</ul></div>";
+
+    for (var i = 1; i < racenames.length; i++) {
+
+        resultaat+="<div id=\"race"+i+"uitleg\" class=\"col s12\">"+jsonRaces[racenames[i]][racenames[i]+" Traits"]["content"]+"</div>\n\n";
+    }
+    resultaat+="</div>";
+    console.log(resultaat);
+    $('div.tabsRacesAfdrukken').html(resultaat);
 }
 
 
